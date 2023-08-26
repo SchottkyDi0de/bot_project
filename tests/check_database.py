d1c5fe70aed9a3fb74d016004696707d1f1feb25
SchottkyDi0de0 <vladislawzero@gmail.com>
2023-08-26 14:49:24 +0300

    test

--- tests/check_database.py
+++ tests/check_database.py
@@ -9,7 +9,6 @@ def print_db():
     db = elara.exe('database/serever_settings.eldb')
     with open('database_copy.json', 'w') as f:
         f.write(json.dumps(db['members'], indent=4))
-    
 
 if __name__ == '__main__':
     print_db()
