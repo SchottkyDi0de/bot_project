65e7aea47774c05338822336350f9185eba515b9
_Zener <vladislawzero@gmail.com>
2023-09-24 00:53:38 +0300

    Fix help command

--- cogs/help.py
+++ cogs/help.py
@@ -1,7 +1,7 @@
 import traceback
 from asyncio import sleep
 
-from discord import Option
+from discord import Option, errors
 from discord.ext import commands
 
 from lib.database.servers import ServersDB
@@ -10,7 +10,7 @@ from lib.embeds.errors import ErrorMSG
 from lib.embeds.info import InfoMSG
 from lib.logger.logger import get_logger
 
-_logger = get_logger(__name__, 'CogHelpLogger', 'logs/cog_help.log')
+_log = get_logger(__name__, 'CogHelpLogger', 'logs/cog_help.log')
 
 
 class Help(commands.Cog):
@@ -31,49 +31,54 @@ class Help(commands.Cog):
         ):
         try:
             Text().load(self.sdb.safe_get_lang(ctx.guild.id))
+            await ctx.defer()
             try:
                 match h_type:
                     case 'syntax':
                         await ctx.user.send(embed=InfoMSG().help_syntax)
-                        ctx.respond(embed=InfoMSG().help_send_ok)
                     case 'setup':
                         await ctx.user.send(embed=InfoMSG().help_setup)
-                        ctx.respond(embed=InfoMSG().help_send_ok)
                     case 'statistics':
                         await ctx.user.send(embed=InfoMSG().help_statistics)
-                        ctx.respond(embed=InfoMSG().help_send_ok)
                     case 'session':
                         await ctx.user.send(embed=InfoMSG().help_session)
-                        ctx.respond(embed=InfoMSG().help_send_ok)
                     case 'all':
                         for i in Text().get().help.types:
                             if i == 'all':
                                 continue
                             await ctx.user.send(embed=getattr(InfoMSG(), f'help_{i}'))
                             await sleep(0.5)
-                        await ctx.respond(embed=InfoMSG().help_send_ok)
                     case _:
                         await ctx.respond(embed=ErrorMSG().unknown_error)
-            except:
-                match h_type:
-                    case 'syntax':
-                        await ctx.respond(embed=InfoMSG().help_syntax)
-                    case 'setup':
-                        await ctx.respond(embed=InfoMSG().help_setup)
-                    case 'statistics':
-                        await ctx.respond(embed=InfoMSG().help_statistics)
-                    case 'session':
-                        await ctx.respond(embed=InfoMSG().help_session)
-                    case 'all':
-                        for i in Text().get().help.types:
-                            if i == 'all':
-                                await ctx.respond(embed=InfoMSG().help_send_ok)
-                            await ctx.user.send(embed=getattr(InfoMSG(), f'help_{i}'))
-                            await sleep(0.5)
-                    case _:
-                        await ctx.respond(embed=ErrorMSG().unknown_error)
+                await ctx.respond(embed=InfoMSG().help_send_ok)
+                return
+            except errors.Forbidden:
+                try:
+                    match h_type:
+                        case 'syntax':
+                            await ctx.channel.send(embed=InfoMSG().help_syntax)
+                        case 'setup':
+                            await ctx.channel.send(embed=InfoMSG().help_setup)
+                        case 'statistics':
+                            await ctx.channel.send(embed=InfoMSG().help_statistics)
+                        case 'session':
+                            await ctx.channel.send(embed=InfoMSG().help_session)
+                        case 'all':
+                            for i in Text().get().help.types:
+                                if i == 'all':
+                                    continue
+                                await ctx.channel.send(embed=getattr(InfoMSG(), f'help_{i}'))
+                                await sleep(0.5)
+                        case _:
+                            await ctx.respond(embed=ErrorMSG().unknown_error)
+                            return
+                except Exception:
+                    await ctx.respond(embed=ErrorMSG().unknown_error)
+                    _log.error(traceback.format_exc())
+                    return
+                await ctx.respond(embed=InfoMSG().help_send_ok)
         except:
-            _logger.error(traceback.format_exc())
+            _log.error(traceback.format_exc())
             await ctx.respond(embed=ErrorMSG().unknown_error)
 
 
