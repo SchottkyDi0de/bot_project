cc115201e73ee592202d64d611e933e8d642211e
ilfv <ilfv183.nt@gmail.com>
2024-02-04 16:43:32 +0200

    add database dump

---
+++ workers/db_backup_worker.py
@@ -0,0 +1,17 @@
+from asyncio import sleep
+
+from lib.database.utils.dump_handler import BackUp
+from lib.logger.logger import get_logger
+
+_log = get_logger(__name__, 'DBBackupWorkerLogger', 'logs/db_backup_worker.log')
+
+
+class DBBackupWorker:
+    def __init__(self):
+        self.STOP_FLAG = False
+    
+    async def run_worker(self):
+        _log.info('WORKERS: DB backup worker started')
+        while not self.STOP_FLAG:
+            BackUp().dump()
+            await sleep(3600 * 24)
